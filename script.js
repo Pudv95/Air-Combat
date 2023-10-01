db8de6367b1ef500552c540f8445cb99b3bec7a2
Pudv95 <vivekupadhayay18@gmail.com>
2023-10-01 18:03:42 +0530

    enable boundries for ground

--- script.js
+++ script.js
@@ -11,13 +11,23 @@ canvas.height = 700;
 const background = new Image();
 background.src = "assets/images/background.png";
 
-// Speed
+/* Speed */
 
-var x_vel1 = 0;
-var y_vel1 = 0;
+// speed
+var speed1 = 5;
+var speed2 = 5;
 
-var x_vel2 = 0; 
-var y_vel2 = 0 
+// For Jet-1
+var x_velocity2_aage = 0;
+var x_velocity2_piche = 0;
+var y_velocity2_aage = 0;
+var y_velocity2_piche = 0;
+
+// For Jet-2
+var x_velocity1_aage = 0;
+var x_velocity1_piche = 0;
+var y_velocity1_aage = 0;
+var y_velocity1_piche = 0;
 
 /* Importing Images */
 
@@ -33,14 +43,14 @@ bullet.src = "assets/images/bullet.png";
 
 
 //inital positions
-var player1 = new Block(jet1,50,310,80,80);
-var player2 = new Block(jet2,950,310,80,80);
-var bulletBlock = new Block(bullet,-10,-10,60,50);
+var player1 = new Block(jet1, 50, 310, 80, 80);
+var player2 = new Block(jet2, 950, 310, 80, 80);
+var bulletBlock = new Block(bullet, -10, -10, 60, 50);
 
 
 
 // Display images on canvas
-function draw(ctx){
+function draw(ctx) {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     player1.draw(ctx);
     player2.draw(ctx);
@@ -48,74 +58,81 @@ function draw(ctx){
 
 
 // Implement Movemets
-function move(){
+function move() {
 
     //player1 Movements;
-    player2.x += x_vel1;
-    player2.y += y_vel1;
+    if(player1.x < 450)player1.x += x_velocity1_aage;
+    if(player1.x > 0) player1.x -= x_velocity1_piche;
+    if(player1.y > 0)player1.y -= y_velocity1_aage;
+    if(player1.y < 630) player1.y += y_velocity1_piche;
 
     //plaer2 Movements;
-    player1.x += x_vel2;
-    player1.y += y_vel2;
+    if(player2.x > 550)player2.x -= x_velocity2_piche;
+    if(player2.x < 1010)player2.x += x_velocity2_aage;
+    if(player2.y > 0)player2.y -= y_velocity2_aage;
+    if(player2.y < 620)player2.y += y_velocity2_piche;
 
-    function handleKeyDown(event){
-        if(event.code == "ArrowUp"){
-            y_vel1 = -5;
+    function handleKeyDown(event) {
+        //player 2
+        if (event.code == "ArrowUp") {
+            y_velocity2_aage = speed2;
         }
-        if(event.code == "ArrowDown"){
-            y_vel1 = 5;
+        if (event.code == "ArrowDown") {
+            y_velocity2_piche = speed2;
         }
-        if(event.code == "ArrowLeft"){
-            x_vel1 = -5;
+        if (event.code == "ArrowLeft") {
+            x_velocity2_piche = speed2;
         }
-        if(event.code == "ArrowRight"){
-            x_vel1 = 5;
+        if (event.code == "ArrowRight") {
+            x_velocity2_aage = speed2;
         }
-        if(event.code == "KeyW"){
-            y_vel2 = -5;
+
+        //player 2
+        if (event.code == "KeyW") {
+            y_velocity1_aage = speed2;
         }
-        if(event.code == "KeyS"){
-            y_vel2 = 5;
+        if (event.code == "KeyS") {
+            y_velocity1_piche = speed2;
         }
-        if(event.code == "KeyA"){
-            x_vel2 = -5;
+        if (event.code == "KeyA") {
+            x_velocity1_piche = speed2;
         }
-        if(event.code == "KeyD"){
-            x_vel2 = 5;
+        if (event.code == "KeyD") {
+            x_velocity1_aage = speed2;
         }
     }
-    function handleKeyUP(event){
-        if(event.code == "ArrowUp"){
-            y_vel1 = 0;
+    function handleKeyUP(event) {
+        if (event.code == "ArrowUp") {
+            y_velocity2_aage = 0;
         }
-        if(event.code == "ArrowDown"){
-            y_vel1 =  0;
+        if (event.code == "ArrowDown") {
+            y_velocity2_piche = 0;
         }
-        if(event.code == "ArrowLeft"){
-            x_vel1 =  0;
+        if (event.code == "ArrowLeft") {
+            x_velocity2_piche = 0;
         }
-        if(event.code == "ArrowRight"){
-            x_vel1 = 0;
+        if (event.code == "ArrowRight") {
+            x_velocity2_aage = 0;
         }
-        if(event.code == "KeyW"){
-            y_vel2 = 0;
+        if (event.code == "KeyW") {
+            y_velocity1_aage = 0;
         }
-        if(event.code == "KeyS"){
-            y_vel2 =  0;
+        if (event.code == "KeyS") {
+            y_velocity1_piche = 0;
         }
-        if(event.code == "KeyA"){
-            x_vel2 = 0;
+        if (event.code == "KeyA") {
+            x_velocity1_piche = 0;
         }
-        if(event.code == "KeyD"){
-            x_vel2 =  0;
+        if (event.code == "KeyD") {
+            x_velocity1_aage = 0;
         }
     }
 
-    document.addEventListener("keydown",handleKeyDown);
-    document.addEventListener("keyup",handleKeyUP);
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUP);
 }
 
-function main(){
+function main() {
     move();
     draw(ctx);
 }
@@ -124,6 +141,6 @@ function main(){
 
 
 
-setInterval(main,1000/60);
+setInterval(main, 1000 / 60);
 
 
